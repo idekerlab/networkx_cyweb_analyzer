9573840d7e9d0def50c3e5141ba74f2c9dd9f02f
Rudi Pillich <rpillich@ucsd.edu>
2025-04-18 15:40:20 -0700

    Update networkxcywebanalyzercmd.py

--- networkxcywebanalyzer/networkxcywebanalyzercmd.py
+++ networkxcywebanalyzer/networkxcywebanalyzercmd.py
@@ -53,7 +53,7 @@ def analyze_network(net_cx2):
 
     net_cx2.add_network_attribute(key='Diameter (Max. Eccentricity)', value=str(max(nx.eccentricity(networkx_graph).values())))
 
-    add_characteristic_path_length_net_attrib(net_cx2)
+    # add_characteristic_path_length_net_attrib(net_cx2)
     
     net_cx2.add_network_attribute(key=' Average Clustering Coefficient', value=str(round(nx.average_clustering(networkx_graph), 3)))
     

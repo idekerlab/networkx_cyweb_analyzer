3ccb9e43334c9fa2f445ae44d8b8d19af812e2c8
Rudi Pillich <rpillich@ucsd.edu>
2025-04-22 12:38:31 -0700

    Update networkxcywebanalyzercmd.py

--- networkxcywebanalyzer/networkxcywebanalyzercmd.py
+++ networkxcywebanalyzer/networkxcywebanalyzercmd.py
@@ -357,7 +357,7 @@ def add_multigraph_unsupported_metrics(net_cx2=None, networkx_graph=None):
     
     # 3. Compute "average clustering coefficient" and "transitivity"
     avg_clustering_coeff = str(round(sum(clustering_coeff.values()) / len(clustering_coeff), 3))
-    transitivity = str(roundnx.transitivity(G_w), 3))
+    transitivity = str(round(nx.transitivity(G_w), 3))
     
     # 4. Set network-level attributes
     net_cx2.add_network_attribute(key=' Average Clustering Coefficient', value=str(avg_clustering_coeff))
